37cc80575bf99699053c4f695b85242b05530c41
nadavdrori <nadavdrori@mail.tau.ac.il>
2024-03-14 13:38:15 +0200

    creat tables work localy

--- create_db_script.py
+++ create_db_script.py
@@ -3,38 +3,38 @@ import mysql.connector as mysql
 
 
 conn = mysql.connect(host='localhost',
-                     user='nadavdrori',
-                     password='nadavdror61892',
-                     db='nadavdrori',
-                     port=3305)
+                     user='root',
+                     password='12345',
+                     db='nadavdb1',
+                     port=3306)
 cursor = conn.cursor()
 
-movies = f"CREATE TABLE Movies (id INTEGER NOT NULL" \
-         f"                     name VARCHAR NOT NULL UNIQUE" \
-         f"                     voteAvg DOUBLE NOT NULL" \
-         f"                     budget INTEGER NOT NULL" \
-         f"                     revenue INTEGER NOT NULL" \
+movies = f"CREATE TABLE Movies (id INTEGER NOT NULL," \
+         f"                     name VARCHAR (100) NOT NULL UNIQUE," \
+         f"                     voteAvg DOUBLE NOT NULL," \
+         f"                     budget INTEGER NOT NULL," \
+         f"                     revenue INTEGER NOT NULL," \
+         f"PRIMARY KEY (id)," \
+         f"CHECK (voteAvg >= 0 and budget >= 0 and revenue >= 0)," \
+         f");" \
+         f"CREATE INDEX movie_name ON Movies(name);"
+
+genre = f"CREATE TABLE Genre (id INTEGER NOT NULL," \
+         f"                   name VARCHAR (100) NOT NULL UNIQUE," \
          f"PRIMARY KEY (id)" \
-         f"CHECK (voteAvg >= 0 and budget >= 0 and revenue >= 0)" \
-         f"CREATE INDEX movie_name ON Movies(name);" \
          f");"
 
-genre = f"CREATE TABLE Genre (id INTEGER NOT NULL" \
-         f"                   name VARCHAR NOT NULL UNIQUE" \
-         f"PRIMARY KEY (id)" \
-         f");"
-
-movie_genre = f"CREATE TABLE MovieGenre (movie_id INTEGER NOT NULL" \
-              f"                          genre_id VARCHAR NOT NULL" \
-              f"FOREIGN KEY (movie_id) REFERENCES Movies(id)" \
+movie_genre = f"CREATE TABLE MovieGenre (movie_id INTEGER NOT NULL," \
+              f"                         genre_id VARCHAR (100) NOT NULL," \
+              f"FOREIGN KEY (movie_id) REFERENCES Movies(id)," \
               f"FOREIGN KEY (genre_id) REFERENCES Genre(id)" \
               f");"
 
-keywords = f"CREATE TABLE Keywords (id INTEGER NOT NULL" \
-           f"                       word VARCHAR NOT NULL UNIQUE" \
-           f"CREATE INDEX key_word ON Keywords(word);" \
+keywords = f"CREATE TABLE Keywords (id INTEGER NOT NULL," \
+           f"                       word VARCHAR (100) NOT NULL UNIQUE," \
            f"PRIMARY KEY (id)" \
-           f");"
+           f");" \
+           f"CREATE INDEX key_word ON Keywords(word);"
 
 movie_keywords = f"CREATE TABLE MovieKeywords (movie_id INTEGER NOT NULL" \
                  f"                            word_id INTEGER NOT NULL" \
@@ -42,14 +42,15 @@ movie_keywords = f"CREATE TABLE MovieKeywords (movie_id INTEGER NOT NULL" \
                  f"FOREIGN KEY (word_id) REFERENCES Keywords(id)" \
                  f");"
 
-cursor.execute(movies)
-rows = cursor.fetchall()
+cursor.execute(movies, multi=True)
+cursor.execute(genre)
+
+"""rows = cursor.fetchall()
 for row in rows:
     print(row)
-cursor.execute(genre)
 cursor.execute(movie_genre)
-cursor.execute(keywords)
-cursor.execute(movie_keywords)
+cursor.execute(keywords, multi=True)
+cursor.execute(movie_keywords)"""
 
 
 cursor.close()
