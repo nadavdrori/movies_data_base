5b826ea3afd681d20b432a4d121f7cafa9b5e9ac
nadavdrori <nadavdrori@mail.tau.ac.il>
2024-03-14 19:04:50 +0200

    examples for queries adn documentation

--- queries_db_script.py
+++ queries_db_script.py
@@ -1,32 +1,26 @@
-import mysql.connector as mysql
 
 
-conn = mysql.connect()
-cursor = conn.cursor()
-
-
-def query_1(word):
+def query_1(keyword):
     """
-        This query retrieves movies associated with a given keyword.
+    The following query retrieves movies associated with the input keyword.
 
-        Parameters: word (str): The keyword to search for.
-        Returns: str: SQL query string.
-        """
+    Parameters: word (str): The keyword to search for.
+    Returns: str: SQL query string.
+    """
 
-    query = f"SELECT DISTINCT Movies.name AS movies with the keyword '{word}'" \
+    query = f"SELECT DISTINCT Movies.name AS movies with the keyword '{keyword}'" \
             f"FROM Keywords, MovieKeywords, Movies" \
-            f"WHERE Keywords.word = '{word}' AND Keywords.id = MovieKeywords.word_id AND " \
+            f"WHERE Keywords.word = '{keyword}' AND Keywords.id = MovieKeywords.word_id AND " \
             f"      MovieKeywords.movie_id = Movies.id"
     return query
 
 
 def query_2(movie_name):
     """
-    This query to retrieves all keywords associated with a given movie.
+    The following query retrieves all keywords associated with the input movie.
 
     Parameters: movie_name (str): The name of the movie to search for.
     Returns: str: SQL query string.
-
     """
     query = f"SELECT Movies.name, Movies.voteAvg, Movies.budget, Movies.revenue, Genre.name, Keywords.word" \
             f"FROM Movies, Genre, MovieGenre, Keywords, MovieKeywords" \
@@ -37,6 +31,15 @@ def query_2(movie_name):
 
 
 def query_3(genre, profit):
+    """
+    The following query retrieves movies of the input genre with a profit equal of above the input profit.
+
+    Parameters:
+    genre (str): The name of the genre to filter movies by.
+    profit (int): The desired profit margin.
+    Returns: str: SQL query string.
+    """
+
     query = f"SELECT Movies.name AS name, Movies.budget AS budget, Movies.revenue AS revenue," \
             f" (Movies.revenue-Movies.budget) AS profit" \
             f"FROM Movies, Genre, MovieGenre" \
@@ -44,18 +47,21 @@ def query_3(genre, profit):
             f"                    FROM Movies, Genre, MovieGenre" \
             f"                    WHERE Genre.name = '{genre}' AND MovieGenre.genre_id = Genre.id AND " \
             f"                          MovieGenre.movie_id = Movies.id) AND " \
-            f"                          (Movies.revenue-Movies.budget) = '{profit}'" \ 
+            f"                          (Movies.revenue-Movies.budget) >= '{profit}'" \
             f"      ORDER BY profit DESC"
     return query
 
 
 def query_4(genre):
+    """
+    The following query calculates the average profit margin for movies from the input genre.
+
+    Parameters: genre (str): The name of the genre to calculate the average profit margin for.
+    Returns: str: SQL query string.
+    """
+
     query = f"SELECT Genre.name AS genre, AVG((Movies.revenue - Movies.budget)) AS profit" \
             f"FROM Movies, Genre, MovieGenre" \
             f"WHERE Genre.name = '{genre}' AND MovieGenre.genre_id = Genre.id AND MovieGenre.movie_id = Movies.id" \
             f"GROUP BY Genre.name"
     return query
-
-
-
-
--- queries_execution.py
+++ queries_execution.py
@@ -1,5 +1,7 @@
-import mysql.connector as mysql
 
+
+import mysql.connector as mysql
+import queries_db_script
 
 
 def main():
@@ -9,6 +11,31 @@ def main():
                          db='nadavdb1',
                          port=3306)
     cursor = conn.cursor()
+
+    # example query no. 1
+    cursor.execute(queries_db_script.query_1('elf'))
+    cursor.execute(queries_db_script.query_1('magic'))
+    cursor.execute(queries_db_script.query_1('fight'))
+    cursor.execute(queries_db_script.query_1('prison'))
+    cursor.execute(queries_db_script.query_1('hero'))
+    cursor.execute(queries_db_script.query_1('family'))
+
+    # example query no. 2
+    cursor.execute(queries_db_script.query_2('The Matrix'))
+    cursor.execute(queries_db_script.query_2('Avatar'))
+    cursor.execute(queries_db_script.query_2('The Hobbit'))
+
+    # example query no. 3
+    cursor.execute(queries_db_script.query_3('horror', 100000000))
+    cursor.execute(queries_db_script.query_3('comedy', 500000000))
+    cursor.execute(queries_db_script.query_3('drama', 125000000))
+    cursor.execute(queries_db_script.query_3('science fiction', 1000000000))
+
+    # example query no. 4
+    cursor.execute(queries_db_script.query_4('horror'))
+    cursor.execute(queries_db_script.query_4('comedy'))
+    cursor.execute(queries_db_script.query_4('drama'))
+    cursor.execute(queries_db_script.query_4('science fiction'))
 
 
 
